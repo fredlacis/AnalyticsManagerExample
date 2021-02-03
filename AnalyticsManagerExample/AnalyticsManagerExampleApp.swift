f4bacb1126c7fcd3c25ff09a464eebf0049c9919
Frederico Lacis <fredlacispro@live.com>
2021-02-03 17:43:28 -0300

    Initial Commit

---
+++ AnalyticsManagerExample/AnalyticsManagerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsManagerExampleApp.swift
+//  AnalyticsManagerExample
+//
+//  Created by Frederico Lacis de Carvalho on 03/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsManagerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
